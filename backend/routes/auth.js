2084bf5eabaed31eaa28f4edbb5b37d242f694b6
Kuraii-dot <dabeh.avee@gmail.com>
2026-03-19 08:34:52 +0800

    removed hashing kineme

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -16,8 +16,3 @@ router.post('/logout', authenticate, logout);
 router.get('/me', authenticate, me);
 
 export default router;
-import bcrypt from 'bcryptjs';
-router.get('/hash/:password', async (req, res) => {
-  const hash = await bcrypt.hash(req.params.password, 10);
-  res.json({ hash });
-});
